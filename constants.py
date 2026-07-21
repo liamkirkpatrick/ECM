06038b1f5e6b092d6fb01643cae9d08cf1fdcd33
Liam Kirkpatrick <liamkirkpatrick@gmail.com>
2026-07-21 13:43:37 -0600

    update motor speeds

--- constants.py
+++ constants.py
@@ -34,8 +34,8 @@ col_spd_AC = 10
 x_spd = 65
 y_spd = 40
 z_spd = 40 # note: due to a change with motores, this doesn't currently work. Use vals below in motor units. Working on it.
-z1_spd_raw = 1800
-z2_spd_raw = 400
+z1_spd_raw = 2000
+z2_spd_raw = 450
 
 #%% Spacing Variables
 
